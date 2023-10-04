760de9efca8de05c4c70a553e1094f8b3d6f3178
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-10-04 23:07:09 +0300

    Update About.tsx

--- src/features/home/About/About.tsx
+++ src/features/home/About/About.tsx
@@ -1,39 +1,25 @@
 'use client';
-import { Container, ThemeProvider, Typography, Box } from '@mui/material';
+import { Container, ThemeProvider, Box } from '@mui/material';
 import { theme } from '@/common/theme';
-import { useAboutData } from './hooks';
-import { AboutData } from './interfaces';
 import { useMedia } from '@/common/hooks';
+import { BgBubbles, AboutText } from './components';
 
 export const About = () => {
-  const data: AboutData = useAboutData();
   const { isSmUp, isMdUp } = useMedia();
 
   return (
     <ThemeProvider theme={theme}>
-      <Box id="about" sx={{ backgroundColor: 'primary.c800' }}>
-        <Container>
-          <Box sx={{ paddingLeft: isMdUp ? '100px' : '0', paddingRight: isMdUp ? '100px' : '0' }}>
-            <Typography
-              variant="h3"
-              component="h3"
-              color="primary.c900"
-              sx={{ marginBottom: isSmUp ? '40px' : '20px', textAlign: 'center' }}
-            >
-              {data.title}
-            </Typography>
-            <Typography
-              variant="p3"
-              component="p"
-              color="primary.c900"
-              sx={{ marginBottom: '40px' }}
-            >
-              {data.text[0]}
-            </Typography>
-            <Typography variant="p3" component="p" color="primary.c900">
-              {data.text[1]}
-            </Typography>
-          </Box>
+      <Box
+        id="about"
+        sx={{ backgroundColor: 'primary.c800', position: 'relative', overflow: 'hidden' }}
+      >
+        <BgBubbles />
+        <Container
+          sx={{
+            py: !isSmUp ? '20px' : isMdUp ? '80px' : '40px',
+          }}
+        >
+          <AboutText />
         </Container>
       </Box>
     </ThemeProvider>

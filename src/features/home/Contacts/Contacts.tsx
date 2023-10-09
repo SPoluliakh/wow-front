848ed69190dc18145f08b96b1802427778ed23a0
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-10-09 22:59:23 +0300

    update contacts cards

--- src/features/home/Contacts/Contacts.tsx
+++ src/features/home/Contacts/Contacts.tsx
@@ -1,14 +1,27 @@
 'use client';
-import { Box, Container } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import { useContacts } from './hooks';
 import { CallBack, ContactsList, Map } from './components';
+import { useMedia } from '@/common/hooks';
 
 export const Contacts = () => {
   const contacts = useContacts();
+  const { isSmUp } = useMedia();
 
   return (
-    <Box id="contacts" sx={{ backgroundColor: 'primary.c800', py: '80px' }}>
+    <Box
+      id="contacts"
+      sx={{ backgroundColor: 'primary.c800', py: { xs: '20px', sm: '64px', md: '80px' } }}
+    >
       <Container>
+        <Typography
+          component="h2"
+          variant={isSmUp ? 'h4' : 'p9'}
+          color="primary.c900"
+          sx={{ textAlign: 'center', mb: { xs: '20px', sm: '40px' } }}
+        >
+          CONTACTS
+        </Typography>
         <ContactsList contacts={contacts} />
         <Map />
         <CallBack />
--- src/features/home/Contacts/components/ContactsList.tsx
+++ src/features/home/Contacts/components/ContactsList.tsx
@@ -1,16 +1,12 @@
 import { Grid } from '@mui/material';
 import { IContacts } from '../interfaces';
 import { ContactsListItem } from './ContactsListItem';
-import { useCartGap, useCartSize } from '../hooks';
 
 interface Props {
   contacts: IContacts[];
 }
 
 export const ContactsList = ({ contacts }: Props) => {
-  const { width, height } = useCartSize();
-  const { rowGap, columnGap } = useCartGap();
-
   return (
     <Grid
       container
@@ -18,8 +14,8 @@ export const ContactsList = ({ contacts }: Props) => {
         alignItems: 'center',
         flexWrap: 'wrap',
         margin: '0 !important',
-        rowGap,
-        columnGap,
+        rowGap: { xs: '20px', sm: '30px', md: '80px' },
+        columnGap: { sm: '30px', md: '20px' },
       }}
     >
       {contacts.map(({ id, title, text, Icon }) => (
@@ -28,15 +24,18 @@ export const ContactsList = ({ contacts }: Props) => {
           key={id}
           sx={{
             p: '0 !important',
+            flexBasis: {
+              xs: '100%',
+              sm: 'calc((100% - 30px * 1) / 2)',
+              md: 'calc((100% - 20px * 2) / 3)',
+            },
+            transition: 'transform 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+            '&:hover': {
+              transform: 'scale(1.01)',
+            },
           }}
         >
-          <ContactsListItem
-            title={title}
-            text={text}
-            icon={<Icon />}
-            width={width}
-            height={height}
-          />
+          <ContactsListItem title={title} text={text} icon={<Icon />} />
         </Grid>
       ))}
     </Grid>

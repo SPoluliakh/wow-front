4a46b38ee6542b252cd1bb35938c486e1974becb
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-10-09 23:01:30 +0300

    Update AboutText.tsx

--- src/features/home/About/components/AboutText.tsx
+++ src/features/home/About/components/AboutText.tsx
@@ -4,50 +4,55 @@ import { IAboutData } from '../interfaces';
 import { useMedia } from '@/common/hooks';
 
 export const AboutText = () => {
-  const data: IAboutData = useAboutData();
-  const { isSmUp, isMdUp } = useMedia();
+  const { title, boldText, plainText }: IAboutData = useAboutData();
+  const { isSmUp } = useMedia();
 
   return (
-    <Box sx={{ px: isMdUp ? '100px' : 0 }}>
+    <Box sx={{ px: { md: '100px' } }}>
       <Typography
-        variant="h4"
+        variant={isSmUp ? 'h4' : 'p9'}
         component="h2"
         color="primary.c900"
-        sx={{ marginBottom: isSmUp ? '40px' : '20px', textAlign: 'center' }}
+        sx={{ marginBottom: { xs: '20px', sm: '40px' }, textAlign: 'center' }}
       >
-        {data.title}
+        {title}
       </Typography>
 
       <Typography
-        variant="p3"
+        variant={isSmUp ? 'p3' : 'p10'}
         component="p"
         color="primary.c900"
-        sx={{ marginBottom: isMdUp ? '40px' : '20px', position: 'relative', zIndex: '5' }}
+        sx={{
+          marginBottom: { xs: '20px', md: '40px' },
+          position: 'relative',
+          zIndex: '5',
+          textAlign: 'justify',
+        }}
       >
-        {data.boldText[0]}
-        <Typography variant="p4" component="span" color="primary.c900">
-          {data.plainText[0]}
+        {boldText[0]}
+        <Typography variant={isSmUp ? 'p4' : 'p5'} component="span" color="primary.c900">
+          {plainText[0]}
         </Typography>
       </Typography>
 
       <Typography
-        variant="p4"
+        variant={isSmUp ? 'p4' : 'p5'}
         component="p"
         color="primary.c900"
-        sx={{ position: 'relative', zIndex: '5' }}
+        sx={{ position: 'relative', zIndex: '5', textAlign: 'justify' }}
       >
-        {data.plainText[1]}
+        {plainText[1]}
 
-        <Typography variant="p3" component="span" color="primary.c900">
-          {data.boldText[1]}
+        <Typography variant={isSmUp ? 'p3' : 'p10'} component="span" color="primary.c900">
+          {boldText[1]}
         </Typography>
 
-        <Typography variant="p4" component="span" color="primary.c900">
-          {data.plainText[2]}
+        <Typography variant={isSmUp ? 'p4' : 'p5'} component="span" color="primary.c900">
+          {plainText[2]}
         </Typography>
 
-        <Typography variant="p3" component="span" color="primary.c900">
-          {data.boldText[2]}
+        <Typography variant={isSmUp ? 'p3' : 'p10'} component="span" color="primary.c900">
+          {boldText[2]}
         </Typography>
       </Typography>
     </Box>

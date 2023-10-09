0b526266d010a87abc558b0f8a9f415fc95736c9
SPoluliakh <S_Poluliakh@ukr.net>
2023-10-09 20:13:31 +0300

    17 add submit object

--- src/common/components/DatePicker/MyDatePicker.tsx
+++ src/common/components/DatePicker/MyDatePicker.tsx
@@ -1,14 +1,16 @@
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { useDate } from './hooks';
 
-export const MyDatePicker = () => {
-  const { selectedDate, handleDateChange } = useDate();
+interface Props {
+  value: Date | null;
+  handleChange: (e: Date | null) => void;
+}
 
+export const MyDatePicker = ({ value, handleChange }: Props) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker value={selectedDate} onChange={handleDateChange} sx={{ width: '100%' }} />
+      <DatePicker value={value} onChange={handleChange} sx={{ width: '100%' }} />
     </LocalizationProvider>
   );
 };
--- src/common/components/Popup/interfaces.ts
+++ src/common/components/Popup/interfaces.ts
@@ -1,5 +1,4 @@
-
-
+import { Dayjs } from 'dayjs';
 export interface SubmitActionsInterface {
   resetForm: () => void;
 }
@@ -13,7 +12,8 @@ export interface BookSubmitValuesInterface {
   location: string;
   name: string;
   phone: string;
-  
+  date: Date | null;
+  time: Dayjs | null;
 }
 
 export type SubmitInterface = (

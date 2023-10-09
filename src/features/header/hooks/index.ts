aae8b64f8c7fc8a266cc9f0b98c9705bd3237d85
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-10-09 22:58:30 +0300

    delete useless hooks

--- src/features/header/hooks/index.ts
+++ src/features/header/hooks/index.ts
@@ -1,2 +1,1 @@
 export * from './useNav';
-export * from './useBtnParameters';
--- src/features/home/Contacts/hooks/index.ts
+++ src/features/home/Contacts/hooks/index.ts
@@ -1,5 +1,2 @@
 export * from './useContacts';
-export * from './useCartSize';
-export * from './useCartGap';
 export * from './useHref';
-export * from './useMapMargin';

cd303ef7c4f9aa6157cbb94fef483963998b1c84
SPoluliakh <S_Poluliakh@ukr.net>
2023-10-13 22:17:39 +0300

    update meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from 'next';
 import { HomeFeature } from '@/features';
 
 export const metadata: Metadata = {
-  title: 'Home | Wow',
-  description: 'Wow description',
+  title: 'Wow | clean&shine',
+  description: 'residential and commercial exterior cleaning services in Aiken and the CSRA.',
 };
 
 export default async function RootPage() {

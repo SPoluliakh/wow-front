d820013cb4b0484e0c5d085baa5e0b3f4e8758df
SPoluliakh <S_Poluliakh@ukr.net>
2023-10-14 18:41:13 +0300

    fix footer styles

--- src/features/footer/FooterFeature.tsx
+++ src/features/footer/FooterFeature.tsx
@@ -2,30 +2,27 @@
 import { theme } from '@/common/theme';
 import { Box, Container, ThemeProvider } from '@mui/material';
 import { Copyright } from './components';
-import { usePosition } from './hooks';
 import { Contacts } from '@/common/components';
 
 export const FooterFeature = () => {
-  const { justifyContent, alignItems, flexDirection, gap, paddingBottom } = usePosition();
-
   return (
     <ThemeProvider theme={theme}>
       <Box
         component="footer"
         sx={{
           backgroundColor: 'primary.c700',
-          paddingTop: '20px',
-          paddingBottom,
+          pt: '20px',
+          pb: { xs: '20px', sm: '40px', md: '80px' },
         }}
       >
         <Container>
           <Box
             sx={{
               display: 'flex',
-              justifyContent,
-              alignItems,
-              flexDirection,
-              gap,
+              justifyContent: { xs: 'center', sm: 'space-between' },
+              alignItems: { xs: 'center', sm: 'flex-start' },
+              flexDirection: { xs: 'column-reverse', sm: 'row' },
+              gap: { xs: '32px', sm: 0 },
             }}
           >
             <Copyright />
